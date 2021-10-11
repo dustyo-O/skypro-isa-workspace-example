1700db53413271338c87a3607d5c8f6700d04128
dusty <dusty@yandex-team.ru>
2021-10-11 10:23:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  var imgElems = document.querySelectorAll('.logged-image');
+  for ( var i=0, len = imgElems.length; i < len; i++ )
+  {
+      var url = imgElems[i].src || imgElems[i].href;
+      if (url && url.length > 0)
+      {
+          var iTime = performance.getEntriesByName(url)[0];
+          console.log(url + ' > ' + iTime.transferSize); //or encodedBodySize, decodedBodySize
+      }
+  }
+});
